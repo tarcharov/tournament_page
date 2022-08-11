309cdb0bdfbb696f7c98705d341003dc2802f5a1
Timofey Arkharov <timofeyarkharov@MacBook-Pro-Timofey.local>
2022-08-11 15:34:49 +0300

    draft

--- src/components/TournamentGrid.js
+++ src/components/TournamentGrid.js
@@ -4,9 +4,17 @@ import styled from "styled-components";
 const TournamentGrid = ({}) => {
   
     const Main = styled.div`
-        display: flex;
-        width: 100%;
-        background-color: red;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+    `
+    const GridWrapper = styled.div`
+      display: flex;
+      justify-content: space-around;
+      background-color: #100649;
+      width: 1113px;
+      height: 665px;
     `
     const Round = styled.div`
       display: flex;
@@ -16,88 +24,111 @@ const TournamentGrid = ({}) => {
       color: white;
     `
     const Match = styled.div`
-        
+    margin: 10px 0px; 
     `
     const TeamWrapper = styled.div`
-    display: flex;
-    margin: 1rem;
-    
+      display: flex;
+      margin: 10px 0px; 
     `
     const Score = styled.div`
-      border: 2px solid orange 
+      background-color: #4C1CF3;
+      margin-left: 3px;
+      width: 36px;
+      height: 40px;
+      border-radius: 0px 5px 5px 0px;
+      text-align: center;
+      padding: 10px;
     `
 
     const Team = styled.div`
-    
-        background-color: #1C0A68;
+      width: 254px;
+      height: 40px;
+      background-color: #1C0A68;
+      border-radius: 5px;
+      text-align: center;
+      padding: 10px;
     `
   
   return (
     <Main>
-      <Round>
-        <Match>
-          <TeamWrapper>
-            <Team>Team1</Team>
-            <Score>12</Score>
-          </TeamWrapper>
-          <TeamWrapper>
-            <Team>Team2</Team>
-            <Score>16</Score>
-          </TeamWrapper>
-        </Match>
-        <Match>
-          <TeamWrapper>
-            <Team>Team1</Team>
-            <Score>12</Score>
-          </TeamWrapper>
-          <TeamWrapper>
-            <Team>Team2</Team>
-            <Score>16</Score>
-          </TeamWrapper>
-        </Match>
-        <Match>
-          <TeamWrapper>
-            <Team>Team1</Team>
-            <Score>16</Score>
-          </TeamWrapper>
-          <TeamWrapper>
-            <Team>Team2</Team>
-            <Score>12</Score>
-          </TeamWrapper>
-        </Match>
-        <Match>
-          <TeamWrapper>
-            <Team>Team1</Team>
-            <Score>9</Score>
-          </TeamWrapper>
-          <TeamWrapper>
-            <Team>Team2</Team>
-            <Score>16</Score>
-          </TeamWrapper>
-        </Match>
-      </Round>
-      <Round>
-        <Match>
-          <TeamWrapper>
-            <Team>Team1</Team>
-            <Score>9</Score>
-          </TeamWrapper>
-          <TeamWrapper>
-            <Team>Team2</Team>
-            <Score>16</Score>
-          </TeamWrapper>
-        </Match>
-        <Match>
-          <TeamWrapper>
-            <Team>Team1</Team>
-            <Score>16</Score>
-          </TeamWrapper>
-          <TeamWrapper>
-            <Team>Team2</Team>
-            <Score>3</Score>
-          </TeamWrapper>
-        </Match>
-      </Round>
+      <GridWrapper>
+        <Round>
+          <Match>
+            <TeamWrapper>
+              <Team>Team1</Team>
+              <Score>12</Score>
+            </TeamWrapper>
+            <TeamWrapper>
+              <Team>Team2</Team>
+              <Score>16</Score>
+            </TeamWrapper>
+          </Match>
+          <Match>
+            <TeamWrapper>
+              <Team>Team1</Team>
+              <Score>12</Score>
+            </TeamWrapper>
+            <TeamWrapper>
+              <Team>Team2</Team>
+              <Score>16</Score>
+            </TeamWrapper>
+          </Match>
+          <Match>
+            <TeamWrapper>
+              <Team>Team1</Team>
+              <Score>16</Score>
+            </TeamWrapper>
+            <TeamWrapper>
+              <Team>Team2</Team>
+              <Score>12</Score>
+            </TeamWrapper>
+          </Match>
+          <Match>
+            <TeamWrapper>
+              <Team>Team1</Team>
+              <Score>9</Score>
+            </TeamWrapper>
+            <TeamWrapper>
+              <Team>Team2</Team>
+              <Score>16</Score>
+            </TeamWrapper>
+          </Match>
+        </Round>
+        <Round>
+          <Match>
+            <TeamWrapper>
+              <Team>Team1</Team>
+              <Score>9</Score>
+            </TeamWrapper>
+            <TeamWrapper>
+              <Team>Team2</Team>
+              <Score>16</Score>
+            </TeamWrapper>
+          </Match>
+          <Match>
+            <TeamWrapper>
+              <Team>Team1</Team>
+              <Score>16</Score>
+            </TeamWrapper>
+            <TeamWrapper>
+              <Team>Team2</Team>
+              <Score>3</Score>
+            </TeamWrapper>
+          </Match>
+        </Round>
+        <Round>
+          <Match>
+            <TeamWrapper>
+              <Team>Team1</Team>
+              <Score>16</Score>
+            </TeamWrapper>
+            <TeamWrapper>
+              <Team>Team2</Team>
+              <Score>3</Score>
+            </TeamWrapper>
+          </Match>
+        </Round>
+      </GridWrapper>
     </Main>
   );
 };
