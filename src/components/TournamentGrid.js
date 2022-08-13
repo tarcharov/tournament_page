e1d62b8ecd3802049caba3e8948dcdab8402badf
Timofey Arkharov <timofeyarkharov@MacBook-Pro-Timofey.local>
2022-08-13 23:15:02 +0300

    tournament grid

--- src/components/TournamentGrid.js
+++ src/components/TournamentGrid.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import db from "./bd.json"
 const TournamentGrid = ({}) => {
   
     const Main = styled.div`
@@ -8,20 +8,20 @@ const TournamentGrid = ({}) => {
       align-items: center;
       justify-content: center;
       width: 100%;
+      background-color: #100649;
     `
     const GridWrapper = styled.div`
       display: flex;
       justify-content: space-around;
-      background-color: #100649;
       width: 1113px;
-      height: 665px;
     `
     const Round = styled.div`
       display: flex;
       flex-direction: column;
       align-items: center;
-      justify-content: center;
+      justify-content: space-around;
       color: white;
+      margin: 0px 20px;
     `
     const Match = styled.div`
     margin: 10px 0px; 
@@ -31,13 +31,14 @@ const TournamentGrid = ({}) => {
       margin: 10px 0px; 
     `
     const Score = styled.div`
-      background-color: #4C1CF3;
+      background-color: ${props => props.winner ? "#025E5F" : "#4C1CF3"};
       margin-left: 3px;
       width: 36px;
       height: 40px;
       border-radius: 0px 5px 5px 0px;
       text-align: center;
       padding: 10px;
+
     `
 
     const Team = styled.div`
@@ -48,86 +49,49 @@ const TournamentGrid = ({}) => {
       text-align: center;
       padding: 10px;
     `
-  
+
+    const MatchWrapper = styled.div`
+      display:flex;
+      justify-content: center;
+      align-items: center;
+    `
+    const stage = Math.log2(db.tournament[0].teams.length)
+    console.log(stage)
+
+    const RoundsLoop = n => {
+      let rounds = []
+        for (let i = 0; i <= n; i++){
+          rounds.push(GenerateStages(i))
+        }
+        return rounds
+    }
+
+    const GenerateStages = n => {
+      return(<Round key={n}>
+        {db.tournament[0].matches.filter(item => item.stage === n).map(item => {
+          return (
+          <MatchWrapper>
+            <Match key={item.id}>
+              <TeamWrapper>
+                <Team>{item.teams[0]}</Team>
+                <Score winner={item.winner === 0}>{item.teamA_score}</Score>
+              </TeamWrapper>
+              <TeamWrapper>
+                <Team>{item.teams[1]}</Team>
+                <Score winner={item.winner === 1}>{item.teamB_score}</Score>
+              </TeamWrapper>
+            </Match>
+            <GorizontalLine/>
+            {item.id % 2 ? <VerticalLineDown/> : <VerticalLineUp/>}
+          </MatchWrapper>
+          )
+        })}
+      </Round>)
+    }
   return (
     <Main>
       <GridWrapper>
-        <Round>
-          <Match>
-            <TeamWrapper>
-              <Team>Team1</Team>
-              <Score>12</Score>
-            </TeamWrapper>
-            <TeamWrapper>
-              <Team>Team2</Team>
-              <Score>16</Score>
-            </TeamWrapper>
-          </Match>
-          <Match>
-            <TeamWrapper>
-              <Team>Team1</Team>
-              <Score>12</Score>
-            </TeamWrapper>
-            <TeamWrapper>
-              <Team>Team2</Team>
-              <Score>16</Score>
-            </TeamWrapper>
-          </Match>
-          <Match>
-            <TeamWrapper>
-              <Team>Team1</Team>
-              <Score>16</Score>
-            </TeamWrapper>
-            <TeamWrapper>
-              <Team>Team2</Team>
-              <Score>12</Score>
-            </TeamWrapper>
-          </Match>
-          <Match>
-            <TeamWrapper>
-              <Team>Team1</Team>
-              <Score>9</Score>
-            </TeamWrapper>
-            <TeamWrapper>
-              <Team>Team2</Team>
-              <Score>16</Score>
-            </TeamWrapper>
-          </Match>
-        </Round>
-        <Round>
-          <Match>
-            <TeamWrapper>
-              <Team>Team1</Team>
-              <Score>9</Score>
-            </TeamWrapper>
-            <TeamWrapper>
-              <Team>Team2</Team>
-              <Score>16</Score>
-            </TeamWrapper>
-          </Match>
-          <Match>
-            <TeamWrapper>
-              <Team>Team1</Team>
-              <Score>16</Score>
-            </TeamWrapper>
-            <TeamWrapper>
-              <Team>Team2</Team>
-              <Score>3</Score>
-            </TeamWrapper>
-          </Match>
-        </Round>
-        <Round>
-          <Match>
-            <TeamWrapper>
-              <Team>Team1</Team>
-              <Score>16</Score>
-            </TeamWrapper>
-            <TeamWrapper>
-              <Team>Team2</Team>
-              <Score>3</Score>
-            </TeamWrapper>
-          </Match>
-        </Round>
+        {RoundsLoop(stage)}
       </GridWrapper>
     </Main>
   );
